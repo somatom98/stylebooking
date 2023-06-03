bb484ec3747cad080af075f1883118e54eab6a97
Tommaso Lauria <tommasolauria98@gmail.com>
2023-06-03 15:12:10 +0200

    feat: read products from mongo db

--- stylebooking_be/api/main.go
+++ stylebooking_be/api/main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func main() {
+
+	// set up mongo client
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb+srv://user:password@free.qko2zsp.mongodb.net/"))
+	if err != nil {
+		panic(err)
+	}
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	servicesCollection := client.Database("stylebooking").Collection("services")
+
 	// set up router
 	router := gin.Default()
 
@@ -21,28 +37,26 @@ func main() {
 	})
 
 	router.GET("/products", func(c *gin.Context) {
-		c.JSON(http.StatusOK, []map[string]string{
-			{
-				"name":        "cut",
-				"description": "cut hair",
-				"price":       "10",
-			},
-			{
-				"name":        "nails",
-				"description": "cut nails",
-				"price":       "5",
-			},
-			{
-				"name":        "wax",
-				"description": "wax hair",
-				"price":       "15",
-			},
-			{
-				"name":        "shave",
-				"description": "shave hair",
-				"price":       "10",
-			},
-		})
+		filter := bson.M{}
+		cur, err := servicesCollection.Find(context.Background(), filter)
+		if err != nil {
+			panic(err)
+		}
+		defer cur.Close(context.Background())
+
+		var services []map[string]interface{}
+		for cur.Next(context.Background()) {
+			var service map[string]interface{}
+			if err := cur.Decode(&service); err != nil {
+				panic(err)
+			}
+			services = append(services, service)
+		}
+		if err := cur.Err(); err != nil {
+			panic(err)
+		}
+
+		c.JSON(http.StatusOK, services)
 	})
 
 	// set up port
